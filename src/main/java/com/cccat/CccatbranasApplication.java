0050830a5eda420fa7b8a490f8504dd2e861da5d
João Victor <joaovictor.cogomes@gmail.com>
2024-01-20 12:26:04 -0300

    Init repository

---
+++ src/main/java/com/cccat/CccatbranasApplication.java
@@ -0,0 +1,13 @@
+package com.cccat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccatbranasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccatbranasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cccat/CccatbranasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cccat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccatbranasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
